8c9e379e8e59f08792177274b842754187a5952a
Ken Broden <brod0268@morris.umn.edu>
2025-04-23 12:12:03 -0500

    rename project to cereal manager

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to cereal_manager
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/cereal_manager/core_test.clj
@@ -0,0 +1,7 @@
+(ns cereal-manager.core-test
+  (:require [clojure.test :refer :all]
+            [cereal-manager.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
