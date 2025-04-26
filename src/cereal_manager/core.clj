5f2b3e6bd96a3de16f09adc0898fd7b0c0976e1c
Ken <brod0268@morris.umn.edu>
2025-04-26 05:06:16 -0500

    load data and sort by a keyword

--- src/cereal_manager/core.clj
+++ src/cereal_manager/core.clj
@@ -1,6 +1,19 @@
-(ns cereal-manager.core)
+(ns cereal-manager.core
+  (:require
+   [clojure.data.csv :as csv] ;; for handling csv files
+   [clojure.java.io :as io])) ;; for file reading
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+;; load the data into a hashmap
+(defn load-data
+  "takes a file path to a .csv file -> returns a hashmap of the data"
+  [file-path]
+  (with-open [reader (io/reader file-path)]
+    (let [rows (doall (csv/read-csv reader))
+          headers (map keyword (first rows))
+          data (rest rows)]
+      (map #(zipmap headers %) data))))
+
+;; test sort by on the loaded data
+(println "sort by calories: \n"
+         (map #(select-keys % [:name :calories])
+               (sort-by #(Integer/parseInt (% :calories)) (load-data "resources/cereal.csv"))))
